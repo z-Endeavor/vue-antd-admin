1c95d03903c1a0ece592b91605374ff6fa52f240
z-Endeavor <zkczdd@163.com>
2024-12-27 11:46:12 +0800

    opt: getSafetyUser

--- admin-backend/src/main/java/com/kevin/adminbackend/service/impl/UserServiceImpl.java
+++ admin-backend/src/main/java/com/kevin/adminbackend/service/impl/UserServiceImpl.java
@@ -124,6 +124,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
      */
     @Override
     public User getSafetyUser(User originUser) {
+        if (originUser == null) {
+            return null;
+        }
         User safetyUser = new User();
         safetyUser.setId(originUser.getId());
         safetyUser.setUsername(originUser.getUsername());
